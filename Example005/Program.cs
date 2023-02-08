a6164af442dbb2566713289514a06d7ae7efc493
Vladislad Dr <despod96@mail.ru>
2023-02-09 00:25:22 +0300

    Example005

--- Example005/Program.cs
+++ Example005/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
 
-if(username == "Маша");
+if(username.ToLower() == "Маша");
 {
     ConsoleWriteLine("Ура это же Маша!");
 }
